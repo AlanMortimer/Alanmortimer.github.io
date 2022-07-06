c833b317b181e067f8b26340816bc61d6020c018
AlanMortimer <76924068+AlanMortimer@users.noreply.github.com>
2022-07-06 17:14:28 +0100

    Update Responsive.js

--- Responsive.js
+++ Responsive.js
@@ -4,6 +4,10 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
  TrackitLogoDiv.style.width="70px"
  TrackitLogoDiv.style.height="70px"
  TrackitLogoDiv.style.borderRadius="70px"
+
+ var TrackitLogo = document.getElementById("TrackitLogo")
+ TrackitLogoDiv.style.width="70px"
+ TrackitLogoDiv.style.height="70px"
 }else{
 
 }
