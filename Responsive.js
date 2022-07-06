4295524638c4fd973cc9e665855a9ce34a3210e1
AlanMortimer <76924068+AlanMortimer@users.noreply.github.com>
2022-07-06 16:53:48 +0100

    Update Responsive.js

--- Responsive.js
+++ Responsive.js
@@ -1,7 +1,9 @@
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-    // true for mobile device
-    document.write("mobile device");
-  }else{
-    // false for not mobile device
-    document.write("not mobile device");
-  }
+   
+ var TrackitLogoDiv = document.getElementById("TrackitLogoDiv")
+ TrackitLogoDiv.style.width="70px"
+ TrackitLogoDiv.style.height="70px"
+ TrackitLogoDiv.style.borderRadius="70px"
+}else{
+
+}
