707fda9aae9202ce108ebe993156365f626d2c65
AlanMortimer <76924068+AlanMortimer@users.noreply.github.com>
2022-07-06 18:26:32 +0100

    Update Responsive.js

--- Responsive.js
+++ Responsive.js
@@ -8,6 +8,52 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     TrackitLogo.style.width="70px"
     TrackitLogo.style.height="70px"
 
+    var HomeDiv = document.getElementById("HomeDiv")
+    HomeDiv.style.width="70px"
+    HomeDiv.style.height="70px"
+    HomeDiv.style.borderRadius="70px"
+   
+    var HomeIcon = document.getElementById("HomeIcon")
+    HomeIcon.style.width="70px"
+    HomeIcon.style.height="70px"
+
+    var LiveDiv = document.getElementById("LiveDiv")
+    LiveDiv.style.width="70px"
+    LiveDiv.style.height="70px"
+    LiveDiv.style.borderRadius="70px"
+   
+    var LiveIcon = document.getElementById("LiveIcon")
+    LiveIcon.style.width="70px"
+    LiveIcon.style.height="70px"
+
+
+    var ScheduleDiv = document.getElementById("ScheduleDiv")
+    ScheduleDiv.style.width="70px"
+    ScheduleDiv.style.height="70px"
+    ScheduleDiv.style.borderRadius="70px"
+   
+    var ScheduleIcon = document.getElementById("ScheduleIcon")
+    ScheduleIcon.style.width="70px"
+    ScheduleIcon.style.height="70px"
+
+
+    var AccountDiv = document.getElementById("AccountDiv")
+    AccountDiv.style.width="70px"
+    AccountDiv.style.height="70px"
+    AccountDiv.style.borderRadius="70px"
+   
+    var AccountIcon = document.getElementById("AccountIcon")
+    AccountIcon.style.width="70px"
+    AccountIcon.style.height="70px"
+
+    var SearchBarDiv = document.getElementById("SearchBarDiv")
+    SearchBarDiv.style.width="70px"
+    SearchBarDiv.style.height="70px"
+    SearchBarDiv.style.borderRadius="70px"
+   
+    var SearchIcon = document.getElementById("SearchIcon")
+    SearchIcon.style.width="70px"
+    SearchIcon.style.height="70px"
 }else{
   
 }
