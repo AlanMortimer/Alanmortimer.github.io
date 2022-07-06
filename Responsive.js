4360f39c5f254ea3688c4c95cce5f9bc00bb5c82
AlanMortimer <76924068+AlanMortimer@users.noreply.github.com>
2022-07-06 22:30:35 +0100

    Update Responsive.js

--- Responsive.js
+++ Responsive.js
@@ -5,52 +5,58 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     var NavBar = document.getElementById("NavBar")
     NavBar.style.position="absolute"
     NavBar.style.bottom="0px"
-    NavBar.style.height="70px"
+    NavBar.style.height="85px"
 
 
 
     var HomeDiv = document.getElementById("HomeDiv")
-    HomeDiv.style.width="70px"
-    HomeDiv.style.height="70px"
-    HomeDiv.style.borderRadius="70px"
-    HomeDiv.style.marginTop= "auto";
+    HomeDiv.style.width="85px"
+    HomeDiv.style.height="85px"
+    HomeDiv.style.borderRadius="85px"
+    HomeDiv.style.marginTop= "auto"
    
     var HomeIcon = document.getElementById("HomeIcon")
     HomeIcon.style.width="70px"
     HomeIcon.style.height="70px"
-    HomeIcon.style.marginLeft="-5px"
-    HomeIcon.style.marginTop="-5px"
+    HomeIcon.style.marginLeft="7.5px"
+    HomeIcon.style.marginTop="2px"
 
     var LiveDiv = document.getElementById("LiveDiv")
-    LiveDiv.style.width="70px"
-    LiveDiv.style.height="70px"
-    LiveDiv.style.borderRadius="70px"
+    LiveDiv.style.width="85px"
+    LiveDiv.style.height="85px"
+    LiveDiv.style.borderRadius="85px"
+    LiveDiv.style.marginTop= "-85px"
    
     var LiveIcon = document.getElementById("LiveIcon")
     LiveIcon.style.width="70px"
     LiveIcon.style.height="70px"
+   
+    LiveIcon.style.marginLeft="7.5px"
 
 
     var ScheduleDiv = document.getElementById("ScheduleDiv")
-    ScheduleDiv.style.width="70px"
-    ScheduleDiv.style.height="70px"
-    ScheduleDiv.style.borderRadius="70px"
+    ScheduleDiv.style.width="85px"
+    ScheduleDiv.style.height="85px"
+    ScheduleDiv.style.borderRadius="85px"
+    ScheduleDiv.style.marginTop="-85px"
    
     var ScheduleIcon = document.getElementById("ScheduleIcon")
-    ScheduleIcon.style.width="70px"
-    ScheduleIcon.style.height="70px"
-
+    ScheduleIcon.style.width="60px"
+    ScheduleIcon.style.height="60px"
+    ScheduleIcon.style.marginLeft="12.5px"
+    ScheduleIcon.style.marginTop="10px"
 
     var AccountDiv = document.getElementById("AccountDiv")
-    AccountDiv.style.width="70px"
-    AccountDiv.style.height="70px"
-    AccountDiv.style.borderRadius="70px"
-   
+    AccountDiv.style.width="85px"
+    AccountDiv.style.height="85px"
+    AccountDiv.style.borderRadius="85px"
+    AccountDiv.style.marginTop="-85px"
     
    
     var AccountIcon = document.getElementById("AccountIcon")
     AccountIcon.style.width="70px"
     AccountIcon.style.height="70px"
+    AccountIcon.style.marginLeft="7.5px"
 
     var SearchBarDiv = document.getElementById("SearchBarDiv")
     SearchBarDiv.style.display="none"
