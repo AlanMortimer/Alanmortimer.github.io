bc7c195bdb6ddb8bbb2df85abb3e012ee3ef2b2e
AlanMortimer <76924068+AlanMortimer@users.noreply.github.com>
2022-07-07 14:18:44 +0100

    Update Responsive.js

--- Responsive.js
+++ Responsive.js
@@ -15,7 +15,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     HomeDiv.style.height="105px"
     HomeDiv.style.borderRadius="105px"
     HomeDiv.style.marginTop= "auto"
-    HomeDiv.style.marginLeft="100px"
+    
    
     var HomeIcon = document.getElementById("HomeIcon")
     HomeIcon.style.width="70px"
@@ -28,7 +28,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     LiveDiv.style.height="105px"
     LiveDiv.style.borderRadius="105px"
     LiveDiv.style.marginTop= "-105px"
-    LiveDiv.style.marginLeft="300px"
+    LiveDiv.style.marginLeft="200px"
    
     var LiveIcon = document.getElementById("LiveIcon")
     LiveIcon.style.width="70px"
@@ -42,7 +42,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     ScheduleDiv.style.height="105px"
     ScheduleDiv.style.borderRadius="105px"
     ScheduleDiv.style.marginTop="-105px"
-    ScheduleDiv.style.marginLeft="500px"
+    ScheduleDiv.style.marginLeft="400px"
     var ScheduleIcon = document.getElementById("ScheduleIcon")
     ScheduleIcon.style.width="60px"
     ScheduleIcon.style.height="60px"
@@ -54,7 +54,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     AccountDiv.style.height="105px"
     AccountDiv.style.borderRadius="105px"
     AccountDiv.style.marginTop="-105px"
-    AccountDiv.style.marginLeft="700px"
+    AccountDiv.style.marginLeft="600px"
    
     var AccountIcon = document.getElementById("AccountIcon")
     AccountIcon.style.width="70px"
@@ -67,7 +67,7 @@ if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigat
     SearchBarDiv.style.height="105px"
     SearchBarDiv.style.borderRadius="105px"
     SearchBarDiv.style.marginTop="-105px"
-    SearchBarDiv.style.marginLeft="900px"
+    SearchBarDiv.style.marginLeft="800px"
     SearchBarDiv.style.backgroundColor="transparent"
     SearchBarDiv.onmouseover = function(){
         SearchBarDiv.style.backgroundColor="#353535"
